59b770bc47573cad1aa256cc49d935b1949a659f
Isobel Barrott <isobel.barrott@gmail.com>
2021-06-11 18:56:50 +0100

    Add data description

--- R/data.R
+++ R/data.R
@@ -1,31 +1,31 @@
-#' Simulated CVD risk assessment data
-#' This is simulated data formed using an LME model.
+#' Simulated repeat measurement data
+#' @description A simulated dataset formed using an LME model, whose parameters were based on CVD risk assessments
+#' recorded at primary care practices in New Zealand. The dataset contains repeat measurements data from 10000 patients.
 #'
-#' @format A dataset containing repeat measurements data from a CVD risk assessment from 10000 patients.
-#'
-#'
-#' A dataset with 30018 rows and 12 columns:
+#' @format A dataset with 30013 rows and 13 columns:
 #' \describe{
 #'   \item{id}{Patient ID}
-#'   \item{smoking}{Smoking status, 0 indicates the patient has 'never smoked', 1 indicated the patient has quit smoking, and 2 indicated the individual is a current smoker.}
+#'   \item{smoking}{Smoking status, 0 indicates the patient has 'never smoked', 1 indicates the patient has quit smoking, and 2 indicated the individual is a current smoker.}
 #'   \item{diabetes}{Diabetes status, 0 indicates the patient does not have diabetes, 1 indicates the patient does have diabetes}
 #'   \item{ethnicity}{Ethnicity, one of five ethnicities}
 #'   \item{deprivation}{Deprivation score out of five}
 #'   \item{index}{An index indicating assessment number for a patient}
-#'   \item{response_date_sbp_stnd}{The date that systolic blood pressure was recorded, this is the same as the date that the fixed measured were recorded}
+#'   \item{response_date_sbp_stnd}{Date that systolic blood pressure was recorded, this is the same as the date that the fixed measures were recorded}
 #'   \item{sbp_stnd}{Standardised systolic blood pressure}
-#'   \item{response_date_tchdl_stnd}{The date that systolic total cholesterol to HDL ratio was recorded}
+#'   \item{response_date_tchdl_stnd}{Date that total cholesterol to HDL ratio was recorded}
 #'   \item{tchdl_stnd}{Standardised total cholesterol to HDL ratio}
 #' }
 #'
 #'
 #'
 "data_repeat"
-#' Simulated CVD event data
+
+#' Simulated time-to-event data
+#' @description A simulated dataset formed using a cause specific model, whose parameters were based on CVD events of patients
+#' at primary care practices in New Zealand. The dataset contains time-to-event data from 10000 patients.
 #'
-#' A dataset containing CVD event data for 4100 patients at risk at landmark age 60. This is simulated data formed using an cause-specific survival model.
+#' @format A dataset with 10000 rows and 3 columns:
 #'
-#' A dataset with 4100 rows and 3 columns:
 #' \describe{
 #'   \item{id}{Patient ID}
 #'   \item{event_status}{Event status, 0 indicates censoring, 1 indicates CVD event, 2 indicates death from other causes}
@@ -33,57 +33,28 @@
 #' }
 #'
 "data_outcomes"
-#' Simulated CVD event data
-#'
-#' A dataset containing CVD event data for 4100 patients at risk at landmark age 60. This is simulated data formed using an cause-specific survival model.
-#'
-#' A dataset with 4100 rows and 3 columns:
-#' \describe{
-#'   \item{id}{Patient ID}
-#'   \item{event_status}{Event status, 0 indicates censoring, 1 indicates CVD event, 2 indicates death from other causes}
-#'   \item{event_time}{Event time}
-#' }
-#'
-"data_landmark"
 
-#' Simulated CVD event data
+#' Simulated data for use in a landmarking analysis
 #'
-#' A dataset containing CVD event data for 4100 patients at risk at landmark age 60. This is simulated data formed using an cause-specific survival model.
+#' A combination of the `data_repeat` and `data_outcomes` data, modified to perform a landmark analysis. See the vignette associated with the `Landmarking`
+#' package (`browseVignettes("Landmarking")`) for more information about how this dataset was formed.
 #'
-#' A dataset with 4100 rows and 3 columns:
-#' \describe{
-#'   \item{id}{Patient ID}
-#'   \item{event_status}{Event status, 0 indicates censoring, 1 indicates CVD event, 2 indicates death from other causes}
-#'   \item{event_time}{Event time}
-#' }
+#'@format A dataset with 22888 rows and 17 columns.
+"data_repeat_outcomes"
+
+#' Simulated data for use in a landmarking analysis
 #'
+#' A combination of the `data_repeat` and `data_outcomes` data, modified to perform a landmark analysis. See the vignette associated with the `Landmarking`
+#' package (`browseVignettes("Landmarking")`) for more information about how this dataset was formed.
+#'
+#'@format A dataset with 22427 rows and 18 columns.
 "data_landmark_cv"
 
 
-#' Simulated CVD event data
+#' Simulated data for use in a landmarking analysis
 #'
-#' A dataset containing CVD event data for 4100 patients at risk at landmark age 60. This is simulated data formed using an cause-specific survival model.
+#' This dataset includes risk predictions for a CVD event. See the vignette associated with the `Landmarking`
+#' package (`browseVignettes("Landmarking")`) for more information about how this dataset was formed.
 #'
-#' A dataset with 4100 rows and 3 columns:
-#' \describe{
-#'   \item{id}{Patient ID}
-#'   \item{event_status}{Event status, 0 indicates censoring, 1 indicates CVD event, 2 indicates death from other causes}
-#'   \item{event_time}{Event time}
-#' }
-#'
-"data_repeat_outcomes"
-
-
-#' Simulated CVD event data
-#'
-#' A dataset containing CVD event data for 4100 patients at risk at landmark age 60. This is simulated data formed using an cause-specific survival model.
-#'
-#' A dataset with 4100 rows and 3 columns:
-#' \describe{
-#'   \item{id}{Patient ID}
-#'   \item{event_status}{Event status, 0 indicates censoring, 1 indicates CVD event, 2 indicates death from other causes}
-#'   \item{event_time}{Event time}
-#' }
-#'
+#' @format A dataset with 6921 rows and 12 columns.
 "data_landmark_LOCF"
-
