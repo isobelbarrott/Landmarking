9b1b2850f47436cb9f925db25c5e795aec65eb7f
Isobel Barrott <isobel.barrott@gmail.com>
2021-06-11 18:56:04 +0100

    Change name of parameter

---
+++ man-roxygen/survival_submodel.R
@@ -0,0 +1,4 @@
+#' @param survival_submodel Character string specifying which survival submodel to
+#' use. Three options: the standard Cox model i.e. no competing risks (\code{"standard_cox"}),
+#' the cause-specific regression model (\code{"cause_specific"}), or the Fine Gray
+#' regression model (\code{"fine_gray"})
