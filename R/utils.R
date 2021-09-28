b1207341c99775d5967dd357528187ba68388eba
Isobel Barrott <isobel.barrott@gmail.com>
2021-09-28 17:56:01 +0100

    Add return_LOCF_by_variable

---
+++ R/utils.R
@@ -0,0 +1,17 @@
+return_LOCF_by_variable <- function(data_long,
+                                    i,
+                                    covariates,
+                                    covariates_time,
+                                    patient_id, x_L) {
+  var <- covariates[i]
+  time <- covariates_time[i]
+  data_var <-
+    data_long[data_long[[time]] < x_L,][, c(patient_id, var, time)]
+  data_var <-
+    data_var[!is.na(data_var[[var]]),]#removes NA values
+  data_var <-
+    data_var[order(data_var[[time]], decreasing = TRUE), ]
+  data_var <-
+    data_var[!duplicated(data_var[[patient_id]]), ]
+  return(data_var[, c(patient_id, var)])
+}
