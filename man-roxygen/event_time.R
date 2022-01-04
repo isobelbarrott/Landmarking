978ce59082fe64c2873ad9fc84475c77adcd54c1
Isobel Barrott <isobel.barrott@gmail.com>
2022-01-04 17:44:41 +0000

    Update description

--- man-roxygen/event_time.R
+++ man-roxygen/event_time.R
@@ -1,2 +1,2 @@
-#' @param event_time Character string specifying the column name in \code{data}
+#' @param event_time Character string specifying the column name in \code{data_long}
 #' which contains the event time
