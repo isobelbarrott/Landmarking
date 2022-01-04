037c7387b754df8670ea691d8ccf89685a251300
Isobel Barrott <isobel.barrott@gmail.com>
2022-01-04 17:35:40 +0000

    Change variable names

---
+++ man-roxygen/individual_id.R
@@ -0,0 +1,1 @@
+#' @param individual_id Character string specifying the column name in \code{data_long} which contains the individual identifiers
--- man-roxygen/patient_id.R
+++
@@ -1,1 +0,0 @@
-#' @param patient_id Character string specifying the column name in \code{data_long}/\code{data} which contains the individual identifiers
