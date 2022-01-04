aff464f7eb294d22c38b546088d0202263f8330d
Isobel Barrott <isobel.barrott@gmail.com>
2022-01-04 17:37:11 +0000

    Add explanation

--- man-roxygen/cross_validation_df.R
+++ man-roxygen/cross_validation_df.R
@@ -1,2 +1,4 @@
-#' @param cross_validation_df List of dataframes containing the cross-validation fold each individual is assigned to. Each dataframe in the list should be
-#' named according to the landmark time x_L that they correspond. Each dataframe should contain the columns \code{patient_id} and a column \code{"cross_validation_number"} which contains the cross-validation fold of the individual.
+#' @param cross_validation_df List of data frames containing the cross-validation fold each individual is assigned to. Each data frame in the list should be
+#' named according to the landmark time `x_L` that they correspond. Each data frame should contain the columns \code{individual_id} and a column \code{cross_validation_number}
+#' which contains the cross-validation fold of the individual. An alternative to setting parameter `k` for performing cross-validation;
+#' if both are missing no cross-validation is used.
