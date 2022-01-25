af2ad110f1842d9d0ef8691d797d0bacda25ea4f
Isobel Barrott <isobel.barrott@gmail.com>
2022-01-25 15:04:19 +0000

    Delete

--- man-roxygen/end_study_time.R
+++
@@ -1,1 +0,0 @@
-#' @param end_study_time Character string specifying the column name in \code{data} which indicates the time that an individual leaves the study
--- man-roxygen/start_study_time.R
+++
@@ -1,1 +0,0 @@
-#' @param start_study_time Character string specifying the column name in \code{data} which indicates the time that an individual enters the study
