ac65a699e7e7c505960b8000e7ccbbab0120cf03
Isobel Barrott <isobel.barrott@gmail.com>
2022-01-30 17:57:07 +0000

    Remove line

--- man-roxygen/event_status.R
+++ man-roxygen/event_status.R
@@ -1,3 +1,3 @@
 #' @param event_status Character string specifying the column name in \code{data_long}
 #' which contains the event status (where 0=censoring, 1=event of interest, if there are competing events these are labelled
-#' 2 or above). Events at time `x_hor` should be labelled censored.
+#' 2 or above).
