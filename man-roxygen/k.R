92c850c648bc69dd8f3122ca430b3580b2421c97
Isobel Barrott <isobel.barrott@gmail.com>
2022-01-30 17:53:42 +0000

    Add line

--- man-roxygen/k.R
+++ man-roxygen/k.R
@@ -1,1 +1,2 @@
-#' @param k Integer specifying the number of folds for cross-validation
+#' @param k Integer specifying the number of folds for cross-validation.
+#' An alternative to setting parameter `cross_validation_df` for performing cross-validation; if both are missing no cross-validation is used.
