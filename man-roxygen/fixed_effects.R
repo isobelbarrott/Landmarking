332162133315b63e8b55149e2745873f63d47107
Isobel Barrott <isobel.barrott@gmail.com>
2022-05-08 14:38:43 +0100

    Clarify description

--- man-roxygen/fixed_effects.R
+++ man-roxygen/fixed_effects.R
@@ -1,2 +1,2 @@
 #' @param fixed_effects Vector of character strings specifying the column names in \code{data_long}
-#' which correspond to the fixed effects
+#' which correspond to the fixed effects variables
