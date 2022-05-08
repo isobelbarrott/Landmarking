22458dbc2eee4a55cad0c659b81d4ab7a163fdf0
Isobel Barrott <isobel.barrott@gmail.com>
2022-05-08 14:21:51 +0100

    Clarify description

--- man-roxygen/random_effects.R
+++ man-roxygen/random_effects.R
@@ -1,2 +1,2 @@
 #' @param random_effects  Vector of character strings specifying the column names in \code{data_long}
-#' which correspond to the random effects
+#' which correspond to the random effects variables
