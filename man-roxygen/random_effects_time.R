6a2ed2887a40829257c4e0003333bc22611e71be
Isobel Barrott <isobel.barrott@gmail.com>
2022-05-08 14:21:28 +0100

    Fix typo

--- man-roxygen/random_effects_time.R
+++ man-roxygen/random_effects_time.R
@@ -1,4 +1,4 @@
 #' @param random_effects_time Vector of character strings specifying the column names in \code{data_long}
-#' which contain the times at which repeat measures were recorded. This should either be length 1 or
+#' which contain the times at which random effect variables were recorded. This should either be length 1 or
 #' the same length as \code{random_effects}. In the latter case the order of elements must correspond to the
 #' order of elements in \code{random_effects}.
