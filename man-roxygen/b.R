2ed0119520c08a9e3e2adfe4d064be92274dd342
Isobel Barrott <isobel.barrott@gmail.com>
2022-05-08 14:26:36 +0100

    Fix typo

--- man-roxygen/b.R
+++ man-roxygen/b.R
@@ -1,1 +1,1 @@
-#' @param b  Integer specifying the number of bootstrap samples to take when calcluating standard error of c-index and Brier score
+#' @param b  Integer specifying the number of bootstrap samples to take when calculating standard error of c-index and Brier score
